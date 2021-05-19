51ff867ff152bf1910d008089a3a02ad273d2bac
Wallace Espindola <wallace.espindola@gmail.com>
2021-05-20 00:37:49 +0200

    Adding tests to key size 1024, 2048, 4096 and 8192

--- src/main/java/com/frankmoley/crypto/asymmetric/AsymmetricEncryptionUtils.java
+++ src/main/java/com/frankmoley/crypto/asymmetric/AsymmetricEncryptionUtils.java
@@ -1,29 +1,31 @@
 package com.frankmoley.crypto.asymmetric;
 
 import javax.crypto.Cipher;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.SecureRandom;
+import java.security.*;
 
 public class AsymmetricEncryptionUtils {
-    private static final String RSA = "RSA";
 
-    public static KeyPair generateRSAKeyPair() throws Exception{
+    private static final String RSA = "RSA";
+    private static final int KEYSIZE_4096 = 4096;
+
+    public static KeyPair generateRSAKeyPair() throws Exception {
+        return generateRSAKeyPair(KEYSIZE_4096);
+    }
+
+    public static KeyPair generateRSAKeyPair(int keySize) throws Exception {
         SecureRandom secureRandom = new SecureRandom();
         KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(RSA);
-        keyPairGenerator.initialize(4096, secureRandom);
+        keyPairGenerator.initialize(keySize, secureRandom);
         return keyPairGenerator.generateKeyPair();
     }
 
-    public static byte[] performRSAEncryption(String plainText, PrivateKey privateKey) throws Exception{
+    public static byte[] performRSAEncryption(String plainText, PrivateKey privateKey) throws Exception {
         Cipher cipher = Cipher.getInstance(RSA);
         cipher.init(Cipher.ENCRYPT_MODE, privateKey);
         return cipher.doFinal(plainText.getBytes());
     }
 
-    public static String performRSADecryption(byte[] cipherText, PublicKey publicKey) throws Exception{
+    public static String performRSADecryption(byte[] cipherText, PublicKey publicKey) throws Exception {
         Cipher cipher = Cipher.getInstance(RSA);
         cipher.init(Cipher.DECRYPT_MODE, publicKey);
         byte[] result = cipher.doFinal(cipherText);
--- src/test/java/com/frankmoley/crypto/asymmetric/AsymmetricEncryptionUtilsTest.java
+++ src/test/java/com/frankmoley/crypto/asymmetric/AsymmetricEncryptionUtilsTest.java
@@ -1,30 +1,134 @@
 package com.frankmoley.crypto.asymmetric;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import javax.xml.bind.DatatypeConverter;
 import java.security.KeyPair;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class AsymmetricEncryptionUtilsTest {
 
     @Test
-    void generateRSAKeyPair() throws Exception{
+    void generateRSAKeyPair_KeySize1024() throws Exception {
+        int keySize = 1024;
+        System.out.println("Using Key Size:  " + keySize);
+        long before = System.currentTimeMillis();
+        KeyPair keyPair = AsymmetricEncryptionUtils.generateRSAKeyPair(keySize);
+        assertNotNull(keyPair);
+        System.out.println("Private Key: " + DatatypeConverter.printHexBinary(keyPair.getPrivate().getEncoded()));
+        System.out.println("Public Key:  " + DatatypeConverter.printHexBinary(keyPair.getPublic().getEncoded()));
+        long after = System.currentTimeMillis();
+        System.out.println("Duration in seconds: " + (after - before) / 1000);
+    }
+
+    @Test
+    void testRSACryptoRoutine_KeySize1024() throws Exception {
+        int keySize = 1024;
+        System.out.println("Using Key Size:  " + keySize);
+        long before = System.currentTimeMillis();
+        KeyPair keyPair = AsymmetricEncryptionUtils.generateRSAKeyPair(keySize);
+        String plainText = "This is the text we are going to hide in plain sight";
+        byte[] cipherText = AsymmetricEncryptionUtils.performRSAEncryption(plainText, keyPair.getPrivate());
+        assertNotNull(cipherText);
+        System.out.println("Plain Text:  " + plainText);
+        System.out.println("Cipher Text: " + DatatypeConverter.printHexBinary(cipherText));
+        String decryptedText = AsymmetricEncryptionUtils.performRSADecryption(cipherText, keyPair.getPublic());
+        assertEquals(plainText, decryptedText);
+        long after = System.currentTimeMillis();
+        System.out.println("Duration in seconds: " + (after - before) / 1000);
+    }
+
+    @Test
+    void generateRSAKeyPair_KeySize2048() throws Exception {
+        int keySize = 2048;
+        System.out.println("Using Key Size:  " + keySize);
+        long before = System.currentTimeMillis();
+        KeyPair keyPair = AsymmetricEncryptionUtils.generateRSAKeyPair(keySize);
+        assertNotNull(keyPair);
+        System.out.println("Private Key: " + DatatypeConverter.printHexBinary(keyPair.getPrivate().getEncoded()));
+        System.out.println("Public Key:  " + DatatypeConverter.printHexBinary(keyPair.getPublic().getEncoded()));
+        long after = System.currentTimeMillis();
+        System.out.println("Duration in seconds: " + (after - before) / 1000);
+    }
+
+    @Test
+    void testRSACryptoRoutine_KeySize2048() throws Exception {
+        int keySize = 2048;
+        System.out.println("Using Key Size:  " + keySize);
+        long before = System.currentTimeMillis();
+        KeyPair keyPair = AsymmetricEncryptionUtils.generateRSAKeyPair(keySize);
+        String plainText = "This is the text we are going to hide in plain sight";
+        byte[] cipherText = AsymmetricEncryptionUtils.performRSAEncryption(plainText, keyPair.getPrivate());
+        assertNotNull(cipherText);
+        System.out.println("Plain Text:  " + plainText);
+        System.out.println("Cipher Text: " + DatatypeConverter.printHexBinary(cipherText));
+        String decryptedText = AsymmetricEncryptionUtils.performRSADecryption(cipherText, keyPair.getPublic());
+        assertEquals(plainText, decryptedText);
+        long after = System.currentTimeMillis();
+        System.out.println("Duration in seconds: " + (after - before) / 1000);
+    }
+
+    @Test
+    void generateRSAKeyPair_KeySize4096() throws Exception {
+        System.out.println("Using Key Size:  4096");
+        long before = System.currentTimeMillis();
         KeyPair keyPair = AsymmetricEncryptionUtils.generateRSAKeyPair();
         assertNotNull(keyPair);
         System.out.println("Private Key: " + DatatypeConverter.printHexBinary(keyPair.getPrivate().getEncoded()));
         System.out.println("Public Key:  " + DatatypeConverter.printHexBinary(keyPair.getPublic().getEncoded()));
+        long after = System.currentTimeMillis();
+        System.out.println("Duration in seconds: " + (after - before) / 1000);
     }
 
     @Test
-    void testRSACryptoRoutine() throws Exception{
+    void testRSACryptoRoutine_KeySize4096() throws Exception {
+        System.out.println("Using Key Size:  4096");
+        long before = System.currentTimeMillis();
         KeyPair keyPair = AsymmetricEncryptionUtils.generateRSAKeyPair();
         String plainText = "This is the text we are going to hide in plain sight";
         byte[] cipherText = AsymmetricEncryptionUtils.performRSAEncryption(plainText, keyPair.getPrivate());
         assertNotNull(cipherText);
-        System.out.println(DatatypeConverter.printHexBinary(cipherText));
+        System.out.println("Plain Text:  " + plainText);
+        System.out.println("Cipher Text: " + DatatypeConverter.printHexBinary(cipherText));
         String decryptedText = AsymmetricEncryptionUtils.performRSADecryption(cipherText, keyPair.getPublic());
         assertEquals(plainText, decryptedText);
+        long after = System.currentTimeMillis();
+        System.out.println("Duration in seconds: " + (after - before) / 1000);
+    }
+
+    @Disabled
+    @Test
+    void generateRSAKeyPair_KeySize8192() throws Exception {
+        int keySize = 8192;
+        System.out.println("Using Key Size:  " + keySize);
+        long before = System.currentTimeMillis();
+        KeyPair keyPair = AsymmetricEncryptionUtils.generateRSAKeyPair(keySize);
+        assertNotNull(keyPair);
+        System.out.println("Private Key: " + DatatypeConverter.printHexBinary(keyPair.getPrivate().getEncoded()));
+        System.out.println("Public Key:  " + DatatypeConverter.printHexBinary(keyPair.getPublic().getEncoded()));
+        long after = System.currentTimeMillis();
+        System.out.println("Duration in seconds: " + (after - before) / 1000);
+    }
+
+    @Disabled
+    @Test
+    void testRSACryptoRoutine_KeySize8192() throws Exception {
+        int keySize = 8192;
+        System.out.println("Using Key Size:  " + keySize);
+        long before = System.currentTimeMillis();
+        KeyPair keyPair = AsymmetricEncryptionUtils.generateRSAKeyPair(keySize);
+        String plainText = "This is the text we are going to hide in plain sight";
+        byte[] cipherText = AsymmetricEncryptionUtils.performRSAEncryption(plainText, keyPair.getPrivate());
+        assertNotNull(cipherText);
+        System.out.println("Plain Text:  " + plainText);
+        System.out.println("Cipher Text: " + DatatypeConverter.printHexBinary(cipherText));
+        String decryptedText = AsymmetricEncryptionUtils.performRSADecryption(cipherText, keyPair.getPublic());
+        assertEquals(plainText, decryptedText);
+        long after = System.currentTimeMillis();
+        System.out.println("Duration in seconds: " + (after - before) / 1000);
     }
 }
