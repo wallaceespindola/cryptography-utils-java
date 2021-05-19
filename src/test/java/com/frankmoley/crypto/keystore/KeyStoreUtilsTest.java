28d69b2c14ff91cf677a0e0048d9d62b48d3d4d9
Wallace Espindola <wallace.espindola@gmail.com>
2021-05-19 23:52:56 +0200

    Initial commit

---
+++ src/test/java/com/frankmoley/crypto/keystore/KeyStoreUtilsTest.java
@@ -0,0 +1,29 @@
+package com.frankmoley.crypto.keystore;
+
+import com.frankmoley.crypto.symmetric.SymmetricEncryptionUtils;
+import org.junit.jupiter.api.Test;
+
+import javax.crypto.SecretKey;
+import javax.xml.bind.DatatypeConverter;
+import java.security.KeyStore;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class KeyStoreUtilsTest {
+
+    @Test
+    void createPrivateKeyJavaKeyStore() throws Exception{
+        SecretKey secretKey = SymmetricEncryptionUtils.createAESKey();
+        String secretKeyHex = DatatypeConverter.printHexBinary(secretKey.getEncoded());
+        KeyStore keyStore = KeyStoreUtils.createPrivateKeyJavaKeyStore("password", "foo", secretKey, "keyPassword");
+        assertNotNull(keyStore);
+
+        keyStore.load(null, "password".toCharArray());
+        KeyStore.ProtectionParameter entryPassword = new KeyStore.PasswordProtection("keyPassword".toCharArray());
+        KeyStore.SecretKeyEntry resultEntry = (KeyStore.SecretKeyEntry) keyStore.getEntry("foo", entryPassword);
+        SecretKey result = resultEntry.getSecretKey();
+        String resultKeyHex = DatatypeConverter.printHexBinary(result.getEncoded());
+        assertEquals(secretKeyHex, resultKeyHex);
+    }
+}
